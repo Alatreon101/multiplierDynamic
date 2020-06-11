9b80a3f5a8bedcde96ebd7afc9a7e520f9994329
FASfalcon <alexanderfedasov@gmail.com>
2020-06-11 03:00:09 +0300

    DMT update1.4 - добавлены переменные шага и стартового числа умножения.

--- src/com/dragon/MultiplicationTable.java
+++ src/com/dragon/MultiplicationTable.java
@@ -10,10 +10,16 @@ public class MultiplicationTable {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число которое хотите умножать: ");
         int number = sc.nextInt();
+        System.out.println("Введите число размерности вычисления: ");
         int size = sc.nextInt();
+        System.out.println("Введите стартовое число с которого будет начинаться умножение: ");
         int startNumber = sc.nextInt();
+        System.out.println("Введите шаг умножения: ");
         int step = sc.nextInt();
+
+        System.out.println("Ваш результат: ");
 
         for (int i = number; i <= number; i += step) {
             for (int j = startNumber; j <= size; j += step) {
